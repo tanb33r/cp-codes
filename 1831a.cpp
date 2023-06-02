5f437daa2c63eee808ff82422ab599cf98fe585d
tanb33r <tanbeer.taher@gmail.com>
2023-06-03 01:15:28 +0600

    asd
    asd

---
+++ 1831a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define  f(i,n)             for(int i=0; i<(n); i++)
+#define  ff(i,a,n)          for(int i=a;i<n;i++)
+#define  fr(i,a,n)          for(int i=a-1;i>=n;i--)
+#define  pb                 push_back
+#define  eb                 emplace_back
+#define  all(v)             (v).begin(), (v).end()
+#define  rall(v)            (v).rbegin(), (v).rend()
+#define  vout(v)            f(I,(v).size())cout<<v[I]<<" ";cout<<"\n"
+#define  AI(a,n)            f(I,n)a[I]=II;
+#define  AO(a,n)            f(I,n)cout<<a[I]<<" ";cout<<"\n"
+#define  F                  first
+#define  S                  second
+#define  ps(x)              cout<<#x<<"\n"
+#define  pr(x)              cout<<x<<"\n"
+#define  vpr(x)             return void(pr(x))
+#define  deb(x)             cerr<<(#x)<<" = "<<x<<"\n"
+#define  hmm(x)             cout<<((x)?"YES":"NO")<<"\n";
+#define  ll                 long long
+#define  II                 ({ll t;cin>>t;t;})
+#define  cbit(n,p)          ((n)&(1LL<<(p)))
+#define  sbit(n,p)          ((n)|(1LL<<(p)))
+#define  tbit(n,p)          ((n)^(1LL<<(p)))
+#define  debb(...)          cerr << "\t[" << #__VA_ARGS__ << "]:\t", dbg_out(__VA_ARGS__)
+//#define  cerr               if(0)cerr
+
+using namespace std;
+void dbg_out() {
+    cerr << endl;
+}
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+typedef pair<int,int> pii;
+const int MAX = 2e5+5;
+
+void solve() {
+    ll n;
+    cin>>n;
+    f(i,n) cout<<n-II+1<<' ';
+    cout<<'\n';
+
+}
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t=1;
+    cin>>t;
+    while(t--)
+        solve();
+}
--- test.cpp
+++ test.cpp
@@ -1,27 +1,22 @@
-#include<cstdio>
-int M(int a,int b) {
-    return a<b?a:b;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve() {
+
+    int n,x;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin>>x;
+        cout<<n-x+1<<' ';
+    }
+    cout << '\n';
+
 }
 int main() {
-    int n,m,i,j,l,r,L,R,l2,b=0;
-    scanf("%d%d",&n,&m);
-    for(i=0; i<n; i++) {
-        l=m+1;
-        r=0;
-        for(j=-1; j<m+2; j++)
-            if(getchar()=='1') {
-                r=j;
-                if(l==m+1)l=j;
-            }
-        l2=M(L+2*r+1,R+m+2);
-        R=M(L+m+2,R+2*(m-l)+3);
-        L=l2;
-        if(r&&!b) {
-            L=r;
-            R=m-l+1;
-            b=1;
-        }
-    }
-    printf("%d\n",b?L:0);
-    return 0;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t=1;
+    cin>>t;
+    while(t--)
+        solve();
 }
+
